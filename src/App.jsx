308066489ceeabc439156e2e7d7206282f1539d1
Daniel Rhodes <danielrhodes47@gmail.com>
2024-08-31 18:27:45 -0500

    implemented delete and add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import TodoList from "./components/TodoList"
 import TodoInput from "./components/Todoinput"
 
@@ -5,11 +6,30 @@ import TodoInput from "./components/Todoinput"
 
 function App() {
 
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index) {
+
+  }
+
   return (
-    <main>
-      <TodoInput />
-      <TodoList />
-    </main>
+    <>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+      <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos} />
+    </>
   )
 }
 
