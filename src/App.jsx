602a19c46992290e02a65d1acb5f8b6bfe495299
Daniel Rhodes <danielrhodes47@gmail.com>
2024-08-31 14:20:46 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import TodoList from "./components/TodoList"
+import TodoInput from "./components/Todoinput"
+
+
+
+function App() {
+
+  return (
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
+  )
+}
+
+export default App
